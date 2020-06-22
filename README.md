b35111caebb37dbebdf839aa67d68e8ccdc78e35
JayChen35 <jay.chen135@gmail.com>
2020-06-21 23:36:08 -0400

    Add finalized script and README.md

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# spotify-to-mp3-python
+# Spotify to MP3 - Python
+
 The simplest way to convert/download your Spotify playlist into MP3 files, using Python 3.
+
+## How To Use
+
+This code is meant to be as simple and easy-to-use as possible. Despite this, there are some setup and usage steps (such as installing necessary packages) that are necessary for this code to work. Please read this section carefully.
+
+### 1. Clone the respository
+
+To clone this repoistory using Git, use
+
+```bash
+git clone https://github.com/JayChen35/spotify-to-mp3-python.git
+```
+
+If you aren't familiar with Git, navigate to the top-left of this page and find the green button labeled "Clone or download". Clicking this and then click "Download ZIP". Extract the contents of the downloaded .zip file.
+
+Open a terminal session and navigate to this folder, using `cd`.
+
+```bash
+cd spotify-to-mp3-python/
+```
+
+### 2. Installing dependencies
+
+We will be installing dependencies using `pip`, the official Python package manager. If you do not have `pip`, I'd recommend checking this [thread](https://stackoverflow.com/questions/6587507/how-to-install-pip-with-python-3/) to install it.
+
+Copy and paste (and run) the following line in your terminal session to install all necessary packages.
+
+```bash
+pip3 install spotipy && pip3 install youtube_dl && pip3 install youtube_search
+```
+
+### 3. Setting up Spotify
+
+Unfortunately, I could not find a workaround for this step - it seems like we're forced to go through the Spotify API to fetch information about playlists. But, it doesn't take long at all.
+
+Go to the Spotify [dashboard](https://developer.spotify.com/dashboard/).  Log in. Once at the Dashboard, click the green button labeled "Create App". Don't worry - you're not signing up for anything or commiting to something from Spotify. Here, **it really doesn't matter what you put** for "App name" and "App description". For me, I just put "Testing" for both. Make sure to check both agreement boxes and click "Create".
+
+You should see this:
+
+![Spotify App Screen](https://miro.medium.com/max/1400/1*8c7agz6nxmez9-bm2NFCxQ.jpeg)
+
+You will see the "Client ID" field on the left (it's redacted here). Copy and save your Client ID somewhere - you'll need it later. Click "Show client secret" under Client ID and it should show you another long list of characters. Also copy and save your Client Secret.
+
+Next, we need your playlist URI. To do this, simply open Spotify, right-click on the playlist you want to download, hover over "Share", and click "Copy Spotify URI". It should look something like this: `spotify:playlist:5UXDjra7SVG5XjdNXW3On2`. When inputting this into the script, make sure to *only input the characters after "spotify:playlist:"*. So for this example, input `5UXDjra7SVG5XjdNXW3On2`. Save your URI somewhere handy
+
+### 4. Running
+
+Running this script is straightforward. Simply run in your terminal session:
+
+```bash
+python3 spotify_to_mp3.py
+```
+
+If you run into an error saying something like "ffprobe or avprobe not found", check out this [solution](https://stackoverflow.com/questions/30770155/ffprobe-or-avprobe-not-found-please-install-one).
+
+If all goes well, you should see your playlist beginning to download in a folder with the same name. Enjoy!
+
+## Modifications
+
+If you don't like inputting your Client ID, Client Secret, Username, and URI  every time, you can edit lines 96-99 in `spotify_to_mp3.py` to set the respective variables into a string containing your credentials instead of prompting with `input()`. For example, line 98 would become
+
+```python
+username = "JayChen35"
+```
+
+## Debugging
+
+This script was made in the better part of an afternoon and so it's not, by far, bug-free. Personally, I've run into no problems using this script on any of my playlists, however, your mileage may vary. The most promenant bug I've run into involves the `youtube-search` package not consistantly turning up results, and most of the time, the best solution is to simply try running the script again and giving it more chances to get the search right.
---
+++ spotify_to_mp3.py
@@ -0,0 +1,110 @@
+# Downloads a Spotify playlist into a folder of MP3 tracks
+# Jason Chen, 21 June 2020
+
+import os
+import spotipy
+import spotipy.oauth2 as oauth2
+import youtube_dl
+from youtube_search import YoutubeSearch
+
+# **************PLEASE READ THE README.md FOR USE INSTRUCTIONS**************
+
+def generate_token(client_id: str, client_secret: str):
+    credentials = oauth2.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    token = credentials.get_access_token()
+    return token
+
+
+def write_tracks(text_file: str, tracks: dict):
+    # Writes the information of all tracks in the playlist to a text file. 
+    # This includins the name, artist, and spotify URL. Each is delimited by a comma.
+    with open(text_file, 'w+', encoding='utf-8') as file_out:
+        while True:
+            for item in tracks['items']:
+                if 'track' in item:
+                    track = item['track']
+                else:
+                    track = item
+                try:
+                    track_url = track['external_urls']['spotify']
+                    track_name = track['name']
+                    track_artist = track['artists'][0]['name']
+                    csv_line = track_name + "," + track_artist + "," + track_url + "\n"
+                    try:
+                        file_out.write(csv_line)
+                    except UnicodeEncodeError:  # Most likely caused by non-English song names
+                        print("Track named {} failed due to an encoding error. This is \
+                            most likely due to this song having a non-English name.".format(track_name))
+                except KeyError:
+                    print(u'Skipping track {0} by {1} (local only?)'.format(
+                            track['name'], track['artists'][0]['name']))
+            # 1 page = 50 results, check if there are more pages
+            if tracks['next']:
+                tracks = spotify.next(tracks)
+            else:
+                break
+
+
+def write_playlist(username: str, playlist_id: str):
+    results = spotify.user_playlist(username, playlist_id, fields='tracks,next,name')
+    playlist_name = results['name']
+    text_file = u'{0}.txt'.format(playlist_name, ok='-_()[]{}')
+    print(u'Writing {0} tracks to {1}.'.format(results['tracks']['total'], text_file))
+    tracks = results['tracks']
+    write_tracks(text_file, tracks)
+    return playlist_name
+
+
+def find_and_download_songs(reference_file: str):
+    TOTAL_ATTEMPTS = 10
+    with open(reference_file, "r", encoding='utf-8') as file:
+        for line in file:
+            temp = line.split(",")
+            name, artist = temp[0], temp[1]
+            text_to_search = artist + " - " + name
+            best_url = None
+            attempts_left = TOTAL_ATTEMPTS
+            while attempts_left > 0:
+                try:
+                    results_list = YoutubeSearch(text_to_search, max_results=1).to_dict()
+                    best_url = "https://www.youtube.com{}".format(results_list[0]['link'])
+                    break
+                except IndexError:
+                    attempts_left -= 1
+                    print("No valid URLs found for {}, trying again ({} attempts left).".format(
+                        text_to_search, attempts_left))
+            if best_url is None:
+                print("No valid URLs found for {}, skipping track.".format(text_to_search))
+                continue
+            # Run you-get to fetch and download the link's audio
+            print("Initiating download for {}.".format(text_to_search))
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([best_url])
+
+
+if __name__ == "__main__":
+    # Parameters
+    print("Please read README.md for use instructions.")
+    client_id = input("Client ID: ")
+    client_secret = input("Client secret: ")
+    username = input("Spotify username: ")
+    playlist_uri = input("Playlist URI (excluding \"spotify:playlist:\"): ")
+    token = generate_token(client_id, client_secret)
+    spotify = spotipy.Spotify(auth=token)
+    playlist_name = write_playlist(username, playlist_uri)
+    reference_file = "{}.txt".format(playlist_name)
+    # Create the playlist folder
+    if not os.path.exists(playlist_name):
+        os.makedirs(playlist_name)
+    os.rename(reference_file, playlist_name + "/" + reference_file)
+    os.chdir(playlist_name)
+    find_and_download_songs(reference_file)
+    print("Operation complete.")
