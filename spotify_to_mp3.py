b7e5c5f21b59e93561ccc4b7305abbf4784bcbad
Everett Periman <ejpman@gmail.com>
2022-02-17 09:53:06 -0500

    Added multicore support
    Added multicore support for the script. Adds an interface that allows for both user interface and for automating the multicore setup parameters. A few tests showed a 6 fold reduction in time on my system (1CPU vs 12CPU). Was able to pull 2756 songs in 23 minutes.

--- spotify_to_mp3.py
+++ spotify_to_mp3.py
@@ -6,6 +6,7 @@ import spotipy
 import spotipy.oauth2 as oauth2
 import yt_dlp
 from youtube_search import YoutubeSearch
+import multiprocessing
 
 # **************PLEASE READ THE README.md FOR USE INSTRUCTIONS**************
 
@@ -84,6 +85,106 @@ def find_and_download_songs(reference_file: str):
                 ydl.download([best_url])
 
 
+# Multiprocessed implementation of find_and_download_songs
+# This method is responsible for manging and distributing the multi-core workload
+def multicore_find_and_download_songs(reference_file: str, cpu_count: int):
+    # Extract songs from the reference file
+    
+    lines = []
+    with open(reference_file, "r", encoding='utf-8') as file:
+        for line in file:
+            lines.append(line)
+
+    # Process allocation of songs per cpu
+    number_of_songs = len(lines)
+    songs_per_cpu = number_of_songs // cpu_count
+
+    # Calculates number of songs that dont evenly fit into the cpu list
+    # i.e. 4 cores and 5 songs, one core will have to process 1 extra song
+    extra_songs = number_of_songs - (cpu_count * songs_per_cpu)
+
+    # Create a list of number of songs which by index allocates it to a cpu
+    # 4 core cpu and 5 songs [2, 1, 1, 1] where each item is the number of songs
+    #                   Core 0^ 1^ 2^ 3^
+    cpu_count_list = []
+    for cpu in range(cpu_count):
+        songs = songs_per_cpu
+        if cpu < extra_songs:
+            songs = songs + 1
+        cpu_count_list.append(songs)
+
+    # Based on the cpu song allocation list split up the reference file
+    index = 0
+    file_segments = []
+    for cpu in cpu_count_list:
+        right = cpu + index
+        segment = lines[index:right]
+        index = index + cpu
+        file_segments.append(segment)
+    
+    # Prepares all of the seperate processes before starting them
+    # Pass each process a new shorter list of songs vs 1 process being handed all of the songs
+    processes = []
+    segment_index = 0
+    for segment in file_segments:
+        p = multiprocessing.Process(target = multicore_handler, args=(segment, segment_index,))
+        processes.append(p)
+        segment_index = segment_index + 1
+
+    # Start the processes
+    for p in processes:
+        p.start()
+
+    # Wait for the processes to complete and exit as a group
+    for p in processes:
+        p.join()
+
+# Just a wrapper around the original find_and_download_songs method to ensure future compatibility
+# Preserves the same functionality just allows for several shorter lists to be used and cleaned up
+def multicore_handler(reference_list: list, segment_index: int):
+    # Create reference filename based off of the process id (segment_index)
+    reference_filename = "{}.txt".format(segment_index)
+    
+    # Write the reference_list to a new "reference_file" to enable compatibility
+    with open(reference_filename, 'w+', encoding='utf-8') as file_out:
+        for line in reference_list:
+            file_out.write(line)
+
+    # Call the original find_and_download method
+    find_and_download_songs(reference_filename)    
+
+    # Clean up the extra list that was generated
+    if(os.path.exists(reference_filename)):
+        os.remove(reference_filename)
+
+
+# This is prompt to handle the multicore queries
+# An effort has been made to create an easily automated interface
+# Autoeneable: bool allows for no prompts and defaults to max core usage
+# Maxcores: int allows for automation of set number of cores to be used
+# Buffercores: int allows for an allocation of unused cores (default 1)
+def enable_multicore(autoenable=False, maxcores=None, buffercores=1):
+    native_cpu_count = multiprocessing.cpu_count() - buffercores
+    if autoenable:
+        if maxcores:
+            if(maxcores <= native_cpu_count):
+                return maxcores
+            else:
+                print("Too many cores requested, single core operation fallback")
+                return 1
+        return multiprocessing.cpu_count() - 1
+    multicore_query = input("Enable multiprocessing (Y or N): ")
+    if multicore_query not in ["Y","y","Yes","YES","YEs",'yes']:
+        return 1
+    core_count_query = int(input("Max core count (0 for allcores): "))
+    if(core_count_query == 0):
+        return native_cpu_count
+    if(core_count_query <= native_cpu_count):
+        return core_count_query
+    else:
+        print("Too many cores requested, single core operation fallback")
+        return 1
+
 if __name__ == "__main__":
     # Parameters
     print("Please read README.md for use instructions.")    
@@ -91,6 +192,7 @@ if __name__ == "__main__":
     client_secret = input("Client secret: ")
     username = input("Spotify username: ")
     playlist_uri = input("Playlist URI (excluding \"spotify:playlist:\"): ")
+    multicore_support = enable_multicore(autoenable=False, maxcores=None, buffercores=1)
     auth_manager = oauth2.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     playlist_name = write_playlist(username, playlist_uri)
@@ -100,5 +202,9 @@ if __name__ == "__main__":
         os.makedirs(playlist_name)
     os.rename(reference_file, playlist_name + "/" + reference_file)
     os.chdir(playlist_name)
-    find_and_download_songs(reference_file)
+    # Enable multicore support
+    if multicore_support > 1:
+        multicore_find_and_download_songs(reference_file, multicore_support)
+    else:
+        find_and_download_songs(reference_file)
     print("Operation complete.")
